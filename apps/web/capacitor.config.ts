86bbf4ebe887b6197ce4775a5f96001eb241d1c6
Adrian Fernández Landivar <adriandfl99@gmail.com>
2026-06-29 11:48:48 -0400

    feat: agregado el favicon.

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.plata_app.app',
   appName: '@plata/web',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://petronovacaribe.com/web/',
+    androidScheme: 'https'
+  }
 };
 
 export default config;
